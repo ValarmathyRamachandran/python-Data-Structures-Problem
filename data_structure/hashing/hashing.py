c385f90a6dc7c5b08a0ec466fc0ca4b7f2f153c3
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-04-04 21:01:33 +0530

    [Valarmathy]:Implemented Data structure programs using python

---
+++ data_structure/hashing/hashing.py
@@ -0,0 +1,44 @@
+class Utility:
+
+    def __init__(self):
+        pass
+
+    def get_int(self):
+        return int(input(""))
+
+
+class HashTable:
+    def __init__(self):
+        pass
+
+    def get_int(self):
+        return int(input(""))
+
+
+def hashing_runner():
+    """
+    This method acts as runner
+    :return: nothing
+    """
+    utility_obj = Utility()
+    hash_obj = HashTable()
+
+    print('These are the Numbers in our File')
+    file = open("../hashing/HashTable File", "r")
+    print(file.readline())
+
+    print('Now enter the Number you are looking for')
+    try:
+        number = utility_obj.get_int()
+    except Exception as e:
+        print(e)
+        print("Enter Number only")
+        hash_obj.insert()
+        print(hash_obj.search(number))
+
+        print('Now Updated File Content are as Follows')
+        hash_obj.file_update(number)
+
+
+if __name__ == "__main__":
+    hashing_runner()
